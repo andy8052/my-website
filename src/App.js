cde66a54f0ec3576eded9c2869fc4fecd5b585bf
andy8052 <andychorlian@gmail.com>
2019-07-22 09:06:14 -0400

    redesigned!

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
+import Terminal from './Terminal'
 
-import Header from './Header';
-import AboutMe from './AboutMe';
-
-const Main = styled.div`
-  height: 100%;
-  padding: 0;
-  margin: 0;
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  font-family: 'Space Mono', monospace;
-`
+const me = {
+  "college": "villanova university",
+  "degree": "computer engineering",
+  "job" : "makerdao",
+  "role" : "smart contract developer",
+  "interests" : ["nba", "blockchain", "video games", "weight lifting"]
+}
 
 class App extends React.Component {
 
   render() {
     return (
-      <Main>
-          <Header/>
-          <AboutMe/>
-      </Main>
+      <Terminal>
+        <kbd>ls -a</kbd>
+        <pre>.skills</pre>
+        <pre><a href="https://github.com/andy8052" target="_blank" rel="noopener noreferrer">github</a></pre>
+        <pre><a href="https://www.linkedin.com/in/achorlia/" target="_blank" rel="noopener noreferrer">linkedin</a></pre>
+        <pre><a href="https://twitter.com/andy8052" target="_blank" rel="noopener noreferrer">twitter</a></pre>
+        <pre><a href="" target="_blank" rel="noopener noreferrer">email</a></pre>
+        <pre><a href="" target="_blank" rel="noopener noreferrer">medium</a></pre>
+        <pre><a href={process.env.PUBLIC_URL+"/resume.pdf"} target="_blank">resume</a></pre>
+        <pre>aboutme.json</pre>
+        <kbd>cat aboutme.json</kbd>
+        <pre>
+          {JSON.stringify(me)}
+        </pre>
+        <kbd>cd .skills | ls -a</kbd>
+        <pre>
+          solidity 
+          k-framework & formal verification
+          java
+          javacript/typescript
+          react.js
+          serverless
+          c#
+        </pre>
+      </Terminal>
     );
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const HeaderInfo = styled.div`
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`
 
 class Header extends React.Component {
 
   render() {
     return (
-      <HeaderInfo>
-          <h2>Andy Chorlian</h2>
-          <h4>Blockchain Engineer @ <a href="https://makerdao.com/en/" target="_blank">MakerDAO</a></h4>
-          <h5><a href={process.env.PUBLIC_URL+"/resume.pdf"} target="_blank">Resume</a></h5>
-      </HeaderInfo>
+      <h3>Andy Chorlian</h3>
     );
   }
 }
