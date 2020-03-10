0b6504fdac4ab6f62c49a4701caba7e6cd692756
andy8052 <andychorlian@gmail.com>
2020-03-10 00:49:16 +0000

    keep it simple stupid

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import me from './andy-lg.png';
-import github from './Octocat.png';
-import linkedin from './linkedin.png';
-import twitter from './Twitter.png';
-import instagram from './Instagram.png';
-import gmail from './gmail.png';
 
 const HeaderInfo = styled.div`
   width:90%;
@@ -19,7 +13,7 @@ const HeaderInfo = styled.div`
   flex-direction: row;
 `
 const Split = styled.div`
-  width:50%;
+  width:80%;
   display: -webkit-box;
   display: -moz-box;
   display: -ms-flexbox;
@@ -29,78 +23,26 @@ const Split = styled.div`
   justify-content: center;
   flex-direction: column;
 `
-const IconRow = styled.div`
-  width:75%;
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-`
-const Icon = styled.div`
-  width:20%;
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-`
-const GitIcon = styled.img`
-  max-width: 80%;
-  max-height: 50px;
-`
-class AboutMe extends React.Component {
 
+class AboutMe extends React.Component {
   render() {
     console.log("url:" + process.env.PUBLIC_URL)
     return (
       <HeaderInfo>
         <Split>
-          <img  src={me} alt="me!"/>
-        </Split>
-        <Split>
-          <h3>About Me</h3>
-          <p>I am a computer nerd and blockchain enthusiast. I graduated with a degree in Computer Engineering in 2016 from Villanova (go cats!).</p>
-          <p>Over the last few years, I have taken a deep dive into blockchain and especially into Ethereum and Solidity development. I am passionate about decentralization and really excited about everything the space has to offer.</p>
-          <p>
-            I love sports and video games. In another life, I would love to work as a stats person in the NBA. I recently wrote a piece about&nbsp;
-            <a href="https://medium.com/we-like-sports/a-deeper-look-into-nba-per-possession-statistics-e5b9be7950f6" target="_blank" rel="noopener noreferrer">NBA Pace</a>
-            &nbsp;which I found really fun to research and write about.
-          </p>
-          <IconRow>
-            <Icon>
-                <a href="https://github.com/andy8052" target="_blank" rel="noopener noreferrer">
-                  <GitIcon  src={github} alt="github"/>
-                </a>
-            </Icon>
-            <Icon>
-              <a href="https://www.linkedin.com/in/achorlia/" target="_blank" rel="noopener noreferrer">
-                <GitIcon  src={linkedin} alt="linkedin"/>
-              </a>
-            </Icon>
-            <Icon>
-              <a href="" target="_blank" rel="noopener noreferrer">
-                <GitIcon  src={gmail} alt="gmail"/>
-              </a>
-            </Icon>
-            <Icon>
-              <a href="https://twitter.com/andy8052" target="_blank" rel="noopener noreferrer">
-                <GitIcon  src={twitter} alt="twitter"/>
-              </a>
-            </Icon>
-            <Icon>
-              <a href="https://www.instagram.com/andy8052/" target="_blank" rel="noopener noreferrer">
-                <GitIcon  src={instagram} alt="instagram"/>
-              </a>
-            </Icon>
-          </IconRow>
-        </Split>
+          <p>----------</p>
+          <p>I spend most my time thinking about blockchain, the NBA, video games and streetwear.</p>
+          <p>I am learning more about cryptography, go and data analytics.</p>
+          <p>----------</p>
+          <p>I do full stack development. Currently I am focused on smart contracts, formal verification and tooling.</p>
+          <p>----------</p>
+          <p>I am always interested in talking about NBA stats. In my free time I dabble in Daily Fantasy Basketball and have my own model for this.</p>
+          <p>I play <a href="https://na.leagueoflegends.com/en/featured/events/teamfight-tactics" target="_blank" rel="noopener noreferrer">Teamfight Tactics</a> competitively with my current peak at rank 50.</p>
+          <p>----------</p>
+          <p>The best way to reach me is on <a href="https://twitter.com/andy8052" target="_blank" rel="noopener noreferrer">Twitter</a>.</p>
+          <p>Otherwise email me at andychorlian(at)gmail(dot)com.</p>
+          <p>Here is my <a href={process.env.PUBLIC_URL+"/resume.pdf"} target="_blank" rel="noopener noreferrer">Resume</a>.</p>
+          </Split>
       </HeaderInfo>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -17,9 +17,8 @@ class Header extends React.Component {
   render() {
     return (
       <HeaderInfo>
-          <h2>Andy Chorlian</h2>
-          <h4>Blockchain Engineer @ <a href="https://makerdao.com/en/" target="_blank">MakerDAO</a></h4>
-          <h5><a href={process.env.PUBLIC_URL+"/resume.pdf"} target="_blank">Resume</a></h5>
+          <h2>👋 Hi I'm Andy</h2>
+          <h4>I'm a Smart Contracts Engineer @ <a href="https://makerdao.com/en/" target="_blank" rel="noopener noreferrer">MakerDAO</a></h4>
       </HeaderInfo>
     );
   }
